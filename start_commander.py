5b9c320332b804c2a14fec37aa769ab4cd6fb190
Sergey Pisarenko <sergey@pisarenko.net>
2020-10-18 17:10:35 +0200

    make daemon settings configurable

--- start_commander.py
+++ start_commander.py
@@ -1,17 +1,22 @@
 from collections import namedtuple
 
 from hifi_appliance.commander import Commander
+from hifi_appliance.config import DAEMON_GROUP
+from hifi_appliance.config import DAEMON_USER
+from hifi_appliance.config import DEBUG
+from hifi_appliance.config import LOG_FILE_COMMANDER
+from hifi_appliance.config import PID_FILE_COMMANDER
 
 
 daemon_config = namedtuple(
     'DaemonConfig',
     ['user', 'group', 'initgroups', 'pid_file', 'log_file']
 )
-daemon_config.user = 'sergey'
-daemon_config.group = 'sergey'
+daemon_config.user = DAEMON_USER
+daemon_config.group = DAEMON_GROUP
 daemon_config.initgroups = False
-daemon_config.pid_file = '/home/sergey/playback.pid'
-daemon_config.log_file = '/home/sergey/playback.log'
+daemon_config.pid_file = PID_FILE_COMMANDER
+daemon_config.log_file = LOG_FILE_COMMANDER
 
 
-commander = Commander(daemon_config, debug=True)
+commander = Commander(daemon_config, debug=DEBUG)
--- start_display.py
+++ start_display.py
@@ -1,17 +1,22 @@
 from collections import namedtuple
 
 from hifi_appliance.display import Display
+from hifi_appliance.config import DAEMON_GROUP
+from hifi_appliance.config import DAEMON_USER
+from hifi_appliance.config import DEBUG
+from hifi_appliance.config import LOG_FILE_DISPLAY
+from hifi_appliance.config import PID_FILE_DISPLAY
 
 
 daemon_config = namedtuple(
     'DaemonConfig',
     ['user', 'group', 'initgroups', 'pid_file', 'log_file']
 )
-daemon_config.user = 'sergey'
-daemon_config.group = 'sergey'
+daemon_config.user = DAEMON_USER
+daemon_config.group = DAEMON_GROUP
 daemon_config.initgroups = False
-daemon_config.pid_file = '/home/sergey/playback.pid'
-daemon_config.log_file = '/home/sergey/playback.log'
+daemon_config.pid_file = PID_FILE_DISPLAY
+daemon_config.log_file = LOG_FILE_DISPLAY
 
 
-display = Display(daemon_config, debug=True)
+display = Display(daemon_config, debug=DEBUG)
