521d1b7981a87c8d1919b6fa05fba549a99d63b6
Sergey Pisarenko <sergey@pisarenko.net>
2020-10-18 21:38:21 +0200

    make yaml load booleans

--- hifi_appliance/config/__init__.py
+++ hifi_appliance/config/__init__.py
@@ -13,7 +13,7 @@ from ..constants import CONFIG_PATH_NAME
 try:
 	with open(CONFIG_PATH_NAME, 'r') as f:
 		this_module = sys.modules[__name__]
-		user_config = yaml.load(f, Loader=yaml.BaseLoader)
+		user_config = yaml.load(f)
 		for key in user_config.keys():
 			setattr(this_module, key, user_config[key])
 except Exception:
